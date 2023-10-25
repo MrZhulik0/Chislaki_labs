3498b09dbf5babc0655ff75c5613fe9b7be82d81
MrZhulik0 <131815262+MrZhulik0@users.noreply.github.com>
2023-10-26 01:11:42 +0300

    NewFiles

---
+++ Header.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void ForwardRunGauss(vector<vector<double>>& table);
+void ReverseRunGauss(vector<vector<double>>& table, vector<double>& solution);
