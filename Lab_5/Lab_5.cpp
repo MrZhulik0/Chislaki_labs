a77693c6570f74e22dd08d3e3ce7a73b5aa31171
Zhulaev Artyom <131815262+MrZhulik0@users.noreply.github.com>
2023-11-25 01:20:27 +0300

    Update Lab_5.cpp

--- Lab_5/Lab_5.cpp
+++ Lab_5/Lab_5.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     // Границы интегрирования
     double a = 0, b = 1.04735;
+    double a1 = 0, b1 = 0.4, c1 = 1.0, d1 = 2.0;
     double eps1 = 1e-4;
     double eps2 = 1e-5;
     // Результаты:
@@ -16,6 +17,7 @@ int main() {
     // Выводим результаты на экран
     cout << "Метод Симпсона при eps = " << eps1 << ": " << res1 << endl;
     cout << "Метод Симпсона при eps = " << eps2 << ": " << res2 << endl;
+    cout << "Кубатурный метод Симпсона:" << integral_xy(a, b, c1, d1, 20, 20) << endl;
     cout << "Метод трапеций при eps = " << eps1 << ": " << res3 << endl;
     cout << "Метод трапеций при eps = " << eps2 << ": " << res4 << endl;
     return 0;
