5b366cbf578fafa5a2571cf0a9e0b735b7ab784f
Zhulaev Artyom <131815262+MrZhulik0@users.noreply.github.com>
2023-12-08 10:56:37 +0300

    Add files via upload

---
+++ Lab_5/func.h
@@ -0,0 +1,5 @@
+double f(double x);
+double F(double x, double y);
+double simpson2d(double a, double b, double c, double d);
+double simpson(double a, double b, double eps);
+double trapezoid(double a, double b, double eps);
