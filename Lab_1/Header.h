457850b3bd92a0d3f9e0f82ea21aa23b81562cfe
Zhulaev Artyom <131815262+MrZhulik0@users.noreply.github.com>
2023-12-09 18:39:15 +0300

    Delete Lab_1 directory

--- Lab_1/Header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-using namespace std;
-
-void ForwardRunGauss(vector<vector<double>>& table);
-void ReverseRunGauss(vector<vector<double>>& table, vector<double>& solution);
