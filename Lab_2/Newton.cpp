b9c336e168db08b01e4f1983abdf46aa7f17f578
Zhulaev Artyom <131815262+MrZhulik0@users.noreply.github.com>
2023-12-14 20:49:58 +0300

    Update Newton.cpp

--- Lab_2/Newton.cpp
+++ Lab_2/Newton.cpp
@@ -11,112 +11,112 @@ double f2(double x, double y) {
     return x*y*y*y- y - 4;
 }
 
-void computeJacobianNumerically(double x, double y, vector<vector <double>>& J, double M) {
-    double f1_val = f1(x, y);
-    double f2_val = f2(x, y);
-    double df1_dx = (f1(x + M, y) - f1_val) / M;
-    double df2_dx = (f2(x + M, y) - f2_val) / M;
-    double df1_dy = (f1(x, M + y) - f1_val) / M;
-    double df2_dy = (f2(x, M + y) - f2_val) / M;
-    J[0][0] = df1_dx;
-    J[0][1] = df1_dy;
-    J[1][0] = df2_dx;
-    J[1][1] = df2_dy;
+void computeJacobianNumerically(double x1, double x2, vector<vector <double>>& J, double M) {
+    J[0][0] = (f1(x1 + x1 * M, x2) - f1(x1, x2)) / (x1 * M);
+    J[0][1] = (f1(x1, x2 + x2 * M) - f1(x1, x2)) / (x2 * M);
+    J[1][0] = (f2(x1 + x1 * M, x2) - f2(x1, x2)) / (x1 * M);
+    J[1][1] = (f2(x1, x2 + x2 * M) - f2(x1, x2)) / (x2 * M);
 }
+
 
 void computeJacobianAnalytically(double x, double y, vector<vector <double>>& J) {
-    double df1_dx = 6*x*x;
-    double df1_dy = 2*y;
-    double df2_dx = y*y*y;
-    double df2_dy = 3*x*y*y - 1;
-    J[0][0] = df1_dx;
-    J[0][1] = df1_dy;
-    J[1][0] = df2_dx;
-    J[1][1] = df2_dy;
+    J[0][0] = 6*x*x;
+    J[0][1] = 2*y;
+    J[1][0] = y*y*y;
+    J[1][1] = 3*x*y*y - 1;
 }
-
 void printNumerically(double M){
     const double e1 = 1e-9;
     const double e2 = 1e-9;
     const int NIT = 100;
-    double x = 1.0;
-    double y = 1.0;
+    double x1 = 1.0;
+    double x2 = -1.0;
     int k = 1;
     cout<<"##############################################################################"<<endl;
     cout<<"M = "<<M<<endl;
-    cout<<"Iteration |" << setw(12) << "x" << setw(6)<<"|"<< setw(12) << "y" << setw(6)<<"|"<< setw(12) << "b1" << setw(6)<<"|" << setw(12)<< "b2" << endl;
+    cout<<"Iteration |" << setw(12) << "x1" << setw(6)<<"|"<< setw(12) << "x2" << setw(6)<<"|"<< setw(12) << "b1" << setw(6)<<"|" << setw(12)<< "b2" << endl;
     while (k <= NIT) {
         vector<vector<double>> J(2, vector<double>(2));
-        computeJacobianNumerically(x, y, J, M);
-        vector<double> F = {f1(x, y), f2(x, y)};
+        computeJacobianNumerically(x1, x2, J, M);
+        vector<double> F = {f1(x1, x2), f2(x1, x2)};
         vector<double> increments(2);
         if(!gauss(J, F, 2, increments))
             break;
-        x-=increments[0];
-        y-=increments[1];
+        x1 -= increments[0];
+        x2 -= increments[1];
+
         double b1 = max(abs(F[0]), abs(F[1]));
-        double b2 = max(abs(increments[0]), abs(increments[1]));
-        b2 = max(b2, abs(increments[0] / x));
-        b2 = max(b2, abs(increments[1] / y)); 
+        double b2;
+        for (int i = 0; i < 2; i++) {
+            if (abs(F[i]) < 1)
+                b2 = max(b2, abs(F[i] - (F[i] - increments[i])));
+            else
+                b2 = max(b2, abs((F[i] - (F[i] - increments[i])) / F[i]));
+        }
+        k++;
         cout << k ;
         if(k<10)
             cout<<setw(10);
         else
             cout<<setw(9);
-        cout<<"|"<<setw(12)<< x << setw(6)<<"|"<< setw(12) << y << setw(6)<<"|"<< setw(12) << b1 << setw(6)<<"|" << setw(12)<< b2 << endl;        
+        cout<<"|"<<setw(12)<< x1 << setw(6)<<"|"<< setw(12) << x2 << setw(6)<<"|"<< setw(12) << b1 << setw(6)<<"|" << setw(12)<< b2 << endl;        
         if (b1 <= e1 && b2 <= e2) {
-            cout << "Converged to the desired precision." << endl;            
+            cout << "Converged to the desired precision." << endl;     
+            cout << "f1 = " << f1(x1, x2)<< endl << "f2 = " << f2(x1, x2)<<endl;       
             break;
         }
         if (k >= NIT) {
+            cout << "f1 = " << f1(x1, x2)<< endl << "f2 = " << f2(x1, x2)<<endl;
             cout << "Iteration limit reached. IER = 2" << endl;
             break;
         }
     k++;
     }
-    cout << "f1("<< x <<","<< y <<") = "<<f1(x, y) << endl;
-    cout << "f2("<< x <<","<< y <<") = "<<f2(x, y) << endl;
 }
 
 void printAnalytically(){
     const double e1 = 1e-9;
     const double e2 = 1e-9;
     const int NIT = 100;
-    double x = 1.0;
-    double y = 1.0;
+    double x1 = 1.0;
+    double x2 = -1.0;
     int k = 1;
     cout<<"##############################################################################"<<endl;
     cout<<"Jacobian Analytically:"<<endl;
-    cout<<"Iteration |" << setw(12) << "x" << setw(6)<<"|"<< setw(12) << "y" << setw(6)<<"|"<< setw(12) << "b1" << setw(6)<<"|" << setw(12)<< "b2" << endl;
+    cout<<"Iteration |" << setw(12) << "x1" << setw(6)<<"|"<< setw(12) << "x2" << setw(6)<<"|"<< setw(12) << "b1" << setw(6)<<"|" << setw(12)<< "b2" << endl;
     while (k <= NIT) {
         vector<vector<double>> J(2, vector<double>(2));
-        computeJacobianAnalytically(x, y, J);
-        vector<double> F = {f1(x, y), f2(x, y)};
+        computeJacobianAnalytically(x1, x2, J);
+        vector<double> F = {f1(x1, x2), f2(x1, x2)};
         vector<double> increments(2);
         if(!gauss(J, F, 2, increments))
             break;
-        x-=increments[0];
-        y-=increments[1];
+        x1 -= increments[0];
+        x2 -= increments[1];
         double b1 = max(abs(F[0]), abs(F[1]));
-        double b2 = max(abs(increments[0]), abs(increments[1]));
-        b2 = max(b2, abs(increments[0] / x));
-        b2 = max(b2, abs(increments[1] / y)); 
+        double b2;
+        for (int i = 0; i < 2; i++) {
+            if (abs(F[i]) < 1)
+                b2 = max(b2, abs(F[i] - (F[i] - increments[i])));
+            else
+                b2 = max(b2, abs((F[i] - (F[i] - increments[i])) / F[i]));
+        } 
         cout << k ;
         if(k<10)
             cout<<setw(10);
         else
             cout<<setw(9);
-        cout<<"|"<<setw(12)<< x << setw(6)<<"|"<< setw(12) << y << setw(6)<<"|"<< setw(12) << b1 << setw(6)<<"|" << setw(12)<< b2 << endl;        
+        cout<<"|"<<setw(12)<< x1 << setw(6)<<"|"<< setw(12) << x2 << setw(6)<<"|"<< setw(12) << b1 << setw(6)<<"|" << setw(12)<< b2 << endl; 
         if (b1 <= e1 && b2 <= e2) {
             cout << "Converged to the desired precision." << endl;            
+            cout << "f1 = " << f1(x1, x2)<< endl << "f2 = " << f2(x1, x2)<<endl;
             break;
         }
         if (k >= NIT) {
+            cout << "f1 = " << f1(x1, x2)<< endl << "f2 = " << f2(x1, x2)<<endl;
             cout << "Iteration limit reached. IER = 2" << endl;
             break;
         }
     k++;
     }
-    cout << "f1("<< x <<","<< y <<") = "<<f1(x, y) << endl;
-    cout << "f2("<< x <<","<< y <<") = "<<f2(x, y) << endl;
 }
